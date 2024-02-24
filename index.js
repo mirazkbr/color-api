8b11d34093fb64fc3342a5acd13ea83f0d32a255
mirazkbr <83450502+mirazkbr@users.noreply.github.com>
2024-02-24 21:07:22 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+const colorCodeSchema = new mongoose.Schema({
+    color: {
+        type: String,
+        required: true,
+        unique: true
+    },
+});
+
+const ColorCode = mongoose.model('ColorCode', colorCodeSchema);
+
 app.post('/colors', async (req, res) => {
     try {
         const { color } = req.body;
@@ -9,6 +40,31 @@ app.post('/colors', async (req, res) => {
         res.status(201).json({ message: 'Color code stored successfully', colorCode: newColorCode });
     } catch (err) {
         console.error('Error storing color code:', err);
+        res.status(500).json({ message: 'Internal server error', error: err.message });
+    }
+});
+
+
+app.get('/mycolors', async (req, res) => {
+    try {
+        const colorCodes = await ColorCode.find();
+        res.status(200).json(colorCodes);
+    } catch (err) {
+        console.error('Error retrieving color codes:', err);
+        res.status(500).json({ message: 'Internal server error GET' });
+    }
+});
+
+app.get('/', async (req, res) => {
+    try {
+        res.send("hello colors");
+    } catch (err) {
+        console.error('Error retrieving color codes:', err);
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
